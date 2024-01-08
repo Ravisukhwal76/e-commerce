28f6f879fe53022b624c3f1abd99d9bc711e3000
ravi sukhwal <113994509+Ravisukhwal76@users.noreply.github.com>
2024-01-08 10:44:10 +0530

    Update GoogleOAuthSuccessHandler.java

--- e-commerce/src/main/java/com/ravi/major/configuration/GoogleOAuthSuccessHandler.java
+++ e-commerce/src/main/java/com/ravi/major/configuration/GoogleOAuthSuccessHandler.java
@@ -1,5 +1,3 @@
-package com.ravi.major.configuration;
-
 import com.ravi.major.entity.Role;
 import com.ravi.major.entity.User;
 import com.ravi.major.repository.RoleRepository;
@@ -12,7 +10,6 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,35 +18,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class GoogleOAuthSuccessHandler  implements AuthenticationSuccessHandler {
+public class GoogleOAuthSuccessHandler implements AuthenticationSuccessHandler {
+
     @Autowired
     RoleRepository roleRepository;
+
     @Autowired
     UserRepository userRepository;
 
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
         String email = token.getPrincipal().getAttribute("email").toString();
-        if(userRepository.findUserByEmail(email).isPresent()){
-
-        }else {
+        
+        if (!userRepository.findUserByEmail(email).isPresent()) {
             User user = new User();
             user.setFirstName(token.getPrincipal().getAttribute("given_name").toString());
             user.setLastName(token.getPrincipal().getAttribute("family_name").toString());
             user.setEmail(email);
             List<Role> roles = new ArrayList<>();
-
             roles.add(roleRepository.findById(2).get());
             user.setRoles(roles);
             userRepository.save(user);
         }
-        redirectStrategy.sendRedirect(request,response,"/");
-     }
-
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-
+        
+        redirectStrategy.sendRedirect(request, response, "/index");
     }
 }
